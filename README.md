55beef14b42c8c041328ab9c220693e06d36e58c
James Porter <james@amimetic.co.uk>
2019-01-28 12:42:43 +0000

    updates

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# JFFS
+# JSON Site Summary
 
 A simple format for single endpoint GET API i.e. a single file which provides everything need for a timeline/feed, events and information about a community/company/service/sports club etc. TypeScript.
+
+Just types at the moment, but I'll probably add some kind of validation for build processes.
 
 ![XKCD on standards](standards.png)
 
--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
 export type Link = {
   kind: "link";
   title: string;
-  subtitle: string;
+  subtitle?: string;
   url: string;
 };
-
 export type Update = {
   kind: "update";
   title: string;
-  subtitle: string;
+  subtitle?: string;
+  extract?: string;
   content: string;
 };
-
 export type Event = {
   title: string;
+  subtitle?: string;
+  description: string;
   dateTime: string;
 };
-
+export type About = {
+  title: string;
+  subtitle?: string;
+  shortDescription: string;
+  imageUrl?: string;
+  color?: string;
+  url?: string;
+};
 export type Content = Link | Update;
-
 export type JFFS = {
-  about: {};
+  about: About;
   updates: Content[];
   events: Event[];
 };
